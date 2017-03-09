6e0b82913e0d9022f3cbfc52bdc081d3fc78ed89
James Langford <derivative7117@live.com>
2017-03-08 20:19:10 -0700

    messaging app

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,22 +1,20 @@
 angular.module('chatroom').controller('mainCtrl', function($scope, messageService){
+    $scope.today = new Date();
+    $scope.getMessage = (function(){
+        messageService.getMessage().then(function(result) {
+            $scope.messages = result.data;
+        });
+    });
+   $scope.postMessage = function (message) {
+       console.log(message);
+       if(message){
+           messageService.postData(message);
+           $scope.message = "";
+       }
+   }
+    setInterval(function(){
+     $scope.getMessage();
 
-  //The getMessages function will call the getMessages method on the messageService object.
-  //You'll then save the result of that request to your controller's $scope as messages ($scope.messages)
-
-
-
-
-  //The postMessage function will take whatever the user typed in (hint: look at the html and see what ng-model correlates to on the input box),
-  //pass that text to the postMessage method on the messageService object which will
-  //then post it to the backend.
-
-
-
-  //uncomment this code when your getMessages function is finished
-  //This goes and gets new data every second, which mimicking a chat room experience.
-  //  setInterval(function(){
-    // $scope.getMessages();
-
-  // }, 1500)
+   }, 1500)
 
 })
